5f9a34e16d0b1d58d6802cbd0c3f25f55fa00c43
Immernachtreich <ebrahimtohfafarosh11@gmail.com>
2022-12-12 13:46:48 +0530

    Added cron

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const CronJob = require('cron').CronJob;
 
 // .env Imports and config
 const dotenv = require('dotenv');
